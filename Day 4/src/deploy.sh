83803b4ee57aabaaae1bbc0ee338e8f1369a0195
Swetha Murugesan <swethamk45@gmail.com>
2025-03-22 14:32:34 +0530

    updated

---
+++ Day 4/src/deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+    # echo hi123
+    sh 'chmod +x build.sh'
+    sh './build.sh'
+    docker login -u swethamurugesan -p 
+    docker tag test1 swethamurugesan/devopsgit
+    docker push swethamurugesan/devopsgit
+    docker-compose up -d    
+c:\Users\Swetha\Documents\DevOps\mydevops\TASK 
